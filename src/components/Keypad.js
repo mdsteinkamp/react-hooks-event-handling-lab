f9c6331df26077c8e9ecbfef4a0082bdb803fa91
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-03-03 19:51:46 -0800

    commit tree a5522c5eb26dfc683c973fce0cbb624bdd78dcea

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,17 @@
 // Code Keypad Component Here
 
 function Keypad (){
+
+    function handleChange(e) {
+        console.log('Entering password...')
+    }
+
     return (
-        <div></div>
+        <div>
+            <form onChange={handleChange}>
+                <input type="password" name="password" placeholder="Enter Password" />
+            </form>
+        </div>
     )
 }
 
